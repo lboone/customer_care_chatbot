91824b7e75a3b47e686fc14ec686d8cdc0db1400
Lloyd Boone <lboone@progressive.com>
2026-03-03 19:26:07 -0500

    feat: integrate Scalekit for authentication and user management
    - Added new API routes for authentication callback and initiating login flow.
    - Implemented user session management with cookies.
    - Created database client and schema for user data using Drizzle ORM.
    - Configured Drizzle for database migrations and schema generation.
    - Added hooks for user state management in the client.
    - Updated package dependencies to include Scalekit and Drizzle ORM.

---
+++ app/api/auth/route.ts
@@ -0,0 +1,30 @@
+import scalekit from "@/lib/scalekit";
+import crypto from "crypto";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const state = crypto.randomBytes(16).toString("hex");
+    (await cookies()).set("sk_state", state, {
+      httpOnly: true,
+      sameSite: "lax",
+      path: "/",
+    });
+    const redirectUri = process.env.SCALEKIT_REDIRECT_URI!;
+
+    const options = {
+      scopes: ["openid", "profile", "email", "offline_access"],
+      state,
+    };
+
+    const authorizationUrl = scalekit.getAuthorizationUrl(redirectUri, options);
+    return NextResponse.redirect(authorizationUrl);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Failed to generate autorization URL" },
+      { status: 500 },
+    );
+  }
+}
--- components/landing/nav/index.tsx
+++ components/landing/nav/index.tsx
@@ -1,7 +1,11 @@
+import { isAuthorized } from "@/lib/isAuthorized";
 import Link from "next/link";
 import Logo from "../logo";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const user = await isAuthorized();
+  console.log(user);
+
   return (
     <nav className="fixed top-0 inset-x-0 z-50 transition-all duration-300 backdrop-blur-sm border-b border-white/5 bg-[#050509]/50">
       <div className="max-w-6xl mx-auto px-6 h-16 flex items-center justify-between">
@@ -22,18 +26,31 @@ const Navbar = () => {
           </Link>
         </div>
         <div className="flex items-center gap-4">
-          <Link
-            href="/api/auth"
-            className="text-xs font-medium text-zinc-400 hover:text-white transition-colors"
-          >
-            Sign in
-          </Link>
-          <Link
-            href="/api/auth"
-            className="text-xs font-medium bg-white  text-black px-4 py-2 rounded-full hover:bg-zinc-200 transition-colors"
-          >
-            Sign Up
-          </Link>
+          {user ? (
+            <div className="flex items-center gap-3">
+              <Link
+                href="/dashboard"
+                className="h-10 px-4 rounded-xl bg-white  text-black text-sm font-medium hover:bg-zinc-200 transition-all flex items-center gap-2"
+              >
+                Dashboard
+              </Link>
+            </div>
+          ) : (
+            <>
+              <Link
+                href="/api/auth"
+                className="text-xs font-medium text-zinc-400 hover:text-white transition-colors"
+              >
+                Sign in
+              </Link>
+              <Link
+                href="/api/auth"
+                className="text-xs font-medium bg-white  text-black px-4 py-2 rounded-full hover:bg-zinc-200 transition-colors"
+              >
+                Get Started!
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </nav>
